b89d4451d9c1109a78b51995e125d2f33cc3b817
Sebcan96 <sebastian.candia96@gmail.com>
2026-01-28 19:11:13 +0100

    removed default css body and buttons

--- frontend-work/src/pages/Home.jsx
+++ frontend-work/src/pages/Home.jsx
@@ -1,21 +1,25 @@
 import Header from "../components/Header.jsx";
 import Searchbar from "../components/Searchbar.jsx";
+import SelectOption from "../components/SelectOption.jsx";
+
+
 
 function Home() {
   return (
     <div className="w-full max-w-6xl mx-auto mt-16 px-6">
 
-      <div className="flex items-center justify-between">
+      <div className="flex items-start mt-70 justify-between">
 
         <Header />
+        <SelectOption />
 
-        <div className=" ml-120 w-100">
+        {/*<div className=" ml-120 w-100">
           <Searchbar />
-        </div>
+        </div>*/}
+        
 
       </div>
-
-      <h2 className="mt-6 text-gray-400">
+      <h2 className="ml-2 text-gray-400">
         Matching help to your solutions
       </h2>
 
