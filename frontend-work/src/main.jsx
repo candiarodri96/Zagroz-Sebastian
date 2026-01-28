ec95732c318ace21c94d9bdd3703c4692c81584c
Turillin <zack.rex1@gmail.com>
2026-01-28 17:21:18 +0100

    small changes

--- frontend-work/src/main.jsx
+++ frontend-work/src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import MainLayout from "./layout/MainLayout.jsx";
 import Home from "./pages/Home.jsx"
+import Login from "./pages/Login.jsx";
 
 
 const router = createBrowserRouter([
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
       },
+      {
+        path: "login",
+        element: <Login></Login>
+      }
     ],
   },
 ]);
