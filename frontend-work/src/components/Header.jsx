3ff2f5d260db6a6bc357fe4bf83889f38b5756b6
Sebcan96 <sebastian.candia96@gmail.com>
2026-01-28 17:21:24 +0100

    commented out searchbar

--- frontend-work/src/components/Header.jsx
+++ frontend-work/src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="py-10 text-center">
       <h1 className="text-6xl font-bold">
-        Work<span className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">Flow</span>
+      <span className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">WorkFlow</span>
         </h1> 
     </header>
   );
