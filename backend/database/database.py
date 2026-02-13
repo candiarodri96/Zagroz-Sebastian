4dae606a66d19207ddfff9a25246db134b4518bb
Sebcan96 <sebastian.candia96@gmail.com>
2026-02-13 10:23:57 +0100

    added database

---
+++ backend/database/database.py
@@ -0,0 +1,15 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+#addera dotenv, import dotenv mm.
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
+Base = declarative_base()
