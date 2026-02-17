571864ce3b6343ec08e78e0f4e33c10b9dfc5a8c
Turillin <zack.rex1@gmail.com>
2026-02-17 19:39:27 +0100

    added user schemas for login and creation with validation

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr, Field
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=8)
+
+class UserCreate(BaseModel):
+    username: str = Field(min_length=3, max_length=30)
+    email: EmailStr
+    password_hash: str = Field(min_length=8)
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: str
+
+    class Config:
+        from_attributes = True
