45900f09f75311100469050cca26e46461057546
Turillin <zack.rex1@gmail.com>
2026-02-17 19:41:08 +0100

    add CORS middleware and update router imports in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
-from routes import router
-from database.database import engine
-from database.models import Base
+from fastapi.middleware.cors import CORSMiddleware
+from routers.users import router as user_router
+from models.models import base
+from db.database import engine
 
+app = FastAPI()
 
-app = FastAPI(
-    
+# Allows for communciation between frontend and backend 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-Base.metadata.create_all(bind=engine)
+# Creates database tables from your models
+base.metadata.create_all(bind=engine)
+
+# Registers the endpoints
+app.include_router(user_router, prefix="/users", tags=["users"])
 
