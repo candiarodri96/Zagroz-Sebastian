5729b9ebbe563d25a7355ddfea25f79dc549d4c6
Turillin <zack.rex1@gmail.com>
2026-02-19 17:56:55 +0100

    Enhance UserCreate schema with first and last name fields and add password validation rules

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,13 +1,32 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, field_validator
+import string
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str = Field(min_length=8)
 
 class UserCreate(BaseModel):
-    username: str = Field(min_length=3, max_length=30)
+    first_name: str = Field(min_length=2, max_length=50)
+    last_name: str = Field(min_length=2, max_length=50)
     email: EmailStr
     password: str = Field(min_length=8)
+
+    @field_validator(password)
+    @classmethod
+    def password_strenght(cls, x):
+        if len(x) < 8:
+            raise ValueError("Password must be longer than 8 characters")
+        if not any(char.isdigit() for char in x):
+            raise ValueError("Password must contain contain at least one digit")
+        if not any(char.isupper() for char in x):
+            raise ValueError("Password must have at least one uppercase letter")
+        if not any(char.islower() for char in x):
+            raise ValueError("Password must contain at least one lowercase letter")
+        if not any(char in string.punctuation for char in x):
+            raise ValueError("Password must contain at least one special character")
+        
+        # hash the password later as well if not already done
+        return x
 
 class UserOut(BaseModel):
     id: int
